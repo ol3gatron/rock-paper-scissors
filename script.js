3af132e7c94e5e3289a73edbb0bf56a6c3fd50e6
ol3gatron <olegatron@gmail.com>
2022-01-02 22:19:52 +0300

    created computer play  function that return rock, paper or scissors

--- script.js
+++ script.js
@@ -1,37 +1,11 @@
-function playGame() {
-    for (i = 0; i < 5; i++) {
-        let playerSelection =  playerPlay().toLowerCase();
-        let computerSelection = computerPlay();
-        
-        function computerPlay() {
-            let rndm = Math.floor(Math.random() * 3 + 1);
-            if (rndm == 1) {
-                return "rock";
-            } else if (rndm == 2) {
-                return "paper";
-            } else if (rndm == 3) {
-                return "scissors";
-            } else {
-                return ""
-            }
-        }
-
-        function playerPlay() {
-            return prompt("Choose and enter 'Rock', 'Paper' or 'Scissors'");
-        }
-
-        function playRound(playerSelection, computerSelection) {
-            if (playerSelection == computerSelection) {
-                return "Tie"
-            } else if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
-                return "You win!";
-            } else {
-                return "You loose :("
-            }
-        }
-
-        console.log(playRound(playerSelection, computerSelection));
-        console.log("You choosed", playerSelection, "and computer choosed", computerSelection);
+function computerPlay() {
+    let result = Math.floor(Math.random() * 3 + 1);
+    if (result === 1) {
+        result = "rock"
+    } else if (result === 2) {
+        result = "paper"
+    } else {
+        result = "scissors"
     }
-}   
-playGame(); 
+    return result;
+}
