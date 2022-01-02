fd10aa0865b55533b54696e3900dfb238dfbc28a
ol3gatron <olegatron@gmail.com>
2022-01-02 22:42:47 +0300

    created a function that plays a round of game and returns result

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerSelection = prompt("Rock, paper or scissors?");
+let computerSelection = computerPlay();
+
 function computerPlay() {
     let result = Math.floor(Math.random() * 3 + 1);
     if (result === 1) {
@@ -9,3 +12,17 @@ function computerPlay() {
     }
     return result;
 }
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Tie!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}.`
+    } else {
+        return `You lose, ${computerSelection} beats ${playerSelection}.`
+    }
+}
+
+console.log("Your choise is " + playerSelection);
+console.log("Computer choise is " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
