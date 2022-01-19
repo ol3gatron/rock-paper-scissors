883e695afb2fc66c3570077701ffe8501fa54b1b
ol3gatron <olegatron@gmail.com>
2022-01-20 01:45:29 +0300

    added final result if someone's score equal to five

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const rpsButtons = document.querySelectorAll("button")
 const choiseDiv = document.querySelector(".choise")
 const resultDiv = document.querySelector(".result")
+const totalResult = document.querySelector(".total-result")
+const score = document.querySelector(".current-score")
+
+let userCounter = 0;
+let compCounter = 0;
 
 function computerPlay() {
     let result = Math.floor(Math.random() * 3 + 1);
@@ -27,12 +32,40 @@ function playRound(playerSelection, computerSelection) {
     }
 }
 
+function counter(a, b) {
+    if (playRound(a, b) === `You win! ${a} beats ${b}🎉`) {
+        userCounter++;
+    } else if (playRound(a,b) === `You lose, ${b} beats ${a}😥`) {
+        compCounter++;
+    }
+}
+
+function getResult(a, b) {
+    if (a === 5) {
+        totalResult.textContent = "Win! 😎";
+        a = 0;
+    } else if (b === 5) {
+        totalResult.textContent = "Lose 😩"
+    }
+}
+
+function endGame(a, b) {
+    if ((a === 5) || (b === 5)) {
+        userCounter = 0;
+        compCounter = 0;
+        totalResult.textContent = ""
+    }
+}
+
 rpsButtons.forEach((button) => {
     button.addEventListener("click", () => {
+        endGame(userCounter, compCounter)
         let computerSelection = computerPlay();
         choiseDiv.textContent = "Your choise is " + button.className + " and " + "computer choise is " + computerSelection;
-        console.log(playRound(button.className, computerSelection));
         resultDiv.textContent = playRound(button.className, computerSelection)
+        counter(button.className, computerSelection)
+        score.textContent = "👧" + userCounter + " - " + compCounter + "🤖"
+        getResult(userCounter, compCounter)
     })
 });
 
